4113e0c63117be3d16b13d929db0f114fa0a7a3f
EvertnnSantts <evertnnsantts@gmail.com>
2025-02-24 15:12:21 -0300

    Atualização: Backend

---
+++ backend/models/ConexaoDB.js
@@ -0,0 +1,16 @@
+import Sequelize from 'sequelize';
+const sequelize = new  Sequelize('opfinance', "root", "82937061eve7iE@", {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false,
+});
+
+  sequelize.authenticate()
+  .then(() => {
+    console.log('conexão realizada com sucesso!');
+  })
+  .catch((err) => {
+     console.log('erro ao conectar com o banco de dados:', err)
+  })
+
+export default sequelize;
