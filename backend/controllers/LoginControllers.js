9c873ffa546d65a78ad385263083da7d030a0d3b
mateusbandeira753 <mateusbandeira753@gmail.com>
2025-02-25 15:35:01 -0300

    Backend atualizado, criado as rotas http

---
+++ backend/controllers/LoginControllers.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const Router = express.Router();
+
+
+Router.get('/users', (req, res) => {
+    res.send('Hello World');
+});
+
+
+
+Router.get('/users/:id', (req, res) => {
+    res.send('Hello World');
+});
+
+
+
+Router.put('/users/:id', (req, res) => {
+    res.send('Hello World');
+});
+
+
+
+Router.delete('/users', (req, res) => {
+    res.send('Hello World');
+});
